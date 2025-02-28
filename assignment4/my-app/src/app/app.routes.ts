69fa0c5204bd93a0bc3de9483ddc71b55c070dbf
Shreyas Walmik Ghodake <shreyasghodake999@gmail.com>
2025-02-28 11:04:56 +0530

    wip

--- assignment4/my-app/src/app/app.routes.ts
+++ assignment4/my-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+export const routes: Routes = [
+    { path: 'registration', component: RegistrationComponent },
+    { path: 'login', component: LoginComponent },
+    {path:'',redirectTo:'login',pathMatch:'full'}
+];
